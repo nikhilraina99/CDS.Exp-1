55f75e144ee2c0da7c0293e0e8c4000c38ea0132
nikhilraina99 <rainanikhilr@gmail.com>
2024-08-12 00:46:21 +0530

    Add files via upload

---
+++ exp1B.cpp
@@ -0,0 +1,26 @@
+//Nikhil
+//23070123093
+//entc b1
+//experiment 1B
+#include <iostream>
+using namespace std;
+int main() 
+{
+
+    int a,b,product, sum,answer;
+    float quotient;
+    cout<<"Enter the value of a:\n";
+    cin>>a;
+    cout<<"Enter the value of b:\n";
+    cin>>b;
+    quotient=a/b;
+    cout<<"The solution of a/b:"<<quotient<<"\n";
+    product=a*b;
+    cout<<"The solution of a*b:"<<product<<"\n";
+    sum=a+b;
+    cout<<"The solution of a+b:"<<sum<<"\n";
+    answer=a-b;
+    cout<<"The solution of a-b:"<<answer<<"\n";
+
+    return 0;
+}
---
+++ exp1c.cpp
@@ -0,0 +1,14 @@
+//Nikhil
+//23070123093
+//entc b1
+//experiment 1B
+#include<iostream>
+using namespace std;
+int main()
+{
+    string a;
+    cout << "Enter your name ";
+    getline(cin,a);
+    cout<<"Hello  " <<a;
+    return 0;
+}
